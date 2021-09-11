64a95f174445ff38d90a0217cfa06f55d730cc58
CKC <chinkuanchen@gmail.com>
2021-09-11 23:50:05 +0800

    basic version done

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 chance = 3
+password = 'a123456'
 while True:
     pw = input('請輸入密碼：')
-    if pw == 'a123456':
+    if pw == password:
         print('成功登入')
         break
-    elif chance > 1:
-        chance = chance - 1
-        print('你還有', chance, '次機會')
     else:
-        print('錯誤三次 帳號鎖定')
-        break
+        chance = chance - 1
+        if chance == 0:
+            print('錯誤三次 帳號鎖定')
+            break
+        else:
+            print('密碼錯誤！ 你還有', chance, '次機會')
 
 
